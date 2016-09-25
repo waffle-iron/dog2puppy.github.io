f8ca5bb5cee24f858041d76eced55a63e7c4992b
Dog2puppy <colecooper2005@icloud.com>
2016-09-25 12:23:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if (localStorage.getItem("firstVisit") == nil) {
   alert("Welcome to my website, visitor!");
-  localStorage.setItem("firstVisit", true);
+  localStorage.setItem("firstVisit", false);
   if (confirm("Would you like to set some local settings??") == true) {
     window.location="//dog2puppy-github.tk/settings";
   }else{
