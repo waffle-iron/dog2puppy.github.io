a21b6f92c5e6d226a100d8eebbc4d35c36470e2a
Dog2puppy <colecooper2005@icloud.com>
2016-09-25 20:20:39 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-if (window.localStorage.getItem("firstVisit")) {
-  window.localStorage.setItem("firstVisit", false);
-  alert("Welcome to my website, visitor!");
-  if (confirm("Would you like to set some local settings??") == true) {
-    window.location="//dog2puppy-github.tk/settings";
-  }else{
-    alert("Okay! You can always use the settings button at the top!");
-  }
-}
